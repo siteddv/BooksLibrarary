aa592124de4fb4b82921cc4f8214c2b297dead49
dmitriivin <dimitrii_vinogradov@epam.com>
2023-03-06 21:07:13 +0600

    added backend with get method

---
+++ BookLibrary.API/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using BooksLibrary.Data.Models.Entities;
+using BooksLibrary.Data.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController
+    {
+        private readonly IRepository<Book> _bookRepository;
+
+        public BookController(IRepository<Book> bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet(Name = "GetAllBooks")]
+        public List<Book> GetBooks()
+        {
+            return _bookRepository.GetAll();
+        }
+    }
+}
---
+++ BookLibrary.API/Program.cs
@@ -0,0 +1,31 @@
+using BooksLibrary.Data.DB.SqlServer;
+using BooksLibrary.Data.Models.Entities;
+using BooksLibrary.Data.Repositories.Implementations;
+using BooksLibrary.Data.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddTransient<IRepository<Book>, Repository<Book>>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
